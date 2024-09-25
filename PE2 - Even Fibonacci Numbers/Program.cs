b1e3e0e49c23aa8749b38b9b3a83afd6d93477ec
Jacob Snyder <95860834+jsGH314@users.noreply.github.com>
2024-09-24 23:14:19 -0500

    Init commit for PE2

---
+++ PE2 - Even Fibonacci Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿//By considering the terms in the Fibonacci sequence whose values do not exceed four million,
+//find the sum of the even-valued terms.
+public class PE2
+{
+    static void Main(string[] args)
+    {
+        PE2 pe2 = new PE2();
+        int sum = pe2.FindSumOfEvenFibonacciNumbers(4000000);
+        Console.WriteLine(sum);
+    }
+
+    public int FindSumOfEvenFibonacciNumbers(int number)
+    {
+        int sum = 0;
+        int counter = 0;
+        while (Fibonacci(counter) < number)
+        {
+            if (Fibonacci(counter) % 2 == 0)
+            {
+                sum += Fibonacci(counter);
+            }
+            counter++;
+        }
+        return sum;
+    }
+    public int Fibonacci(int number) => number == 0 ? 0 : number == 1 ? 1 : Fibonacci(number - 1) + Fibonacci(number - 2);
+}
