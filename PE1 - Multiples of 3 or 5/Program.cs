9da6283e4fc2c858b38cab46b2811f245766bfb7
Jacob Snyder <95860834+jsGH314@users.noreply.github.com>
2024-09-24 22:32:51 -0500

    Add project files.

---
+++ PE1 - Multiples of 3 or 5/Program.cs
@@ -0,0 +1,30 @@
+﻿//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+//The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+public class PE1
+{
+    public static void Main()
+    {
+        PE1 pe = new PE1();
+        Console.WriteLine(pe.FindSumOfMultiples(1000));
+    }
+
+    public int FindSumOfMultiples(int number)
+    {
+        int sum = 0;
+        for (int i = 0; i < number; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0)
+            {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
+
+
+
+
+
