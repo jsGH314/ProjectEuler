e8fe2ce8f2c3bc3a7bf04e7ba8b6b78000ed80e0
Jacob Snyder <95860834+jsGH314@users.noreply.github.com>
2024-09-25 10:44:24 -0500

    init commit for PE4

---
+++ PE4 - Largest Palindrome Product/Program.cs
@@ -0,0 +1,46 @@
+﻿public class PE4
+{
+    static void Main(string[] args)
+    {
+        PE4 pe4 = new PE4();
+        string result = pe4.GetLargestPalindromeProduct();
+        Console.WriteLine(result);
+    }
+
+    public string GetLargestPalindromeProduct()
+    {
+        int largestProduct = 0;
+        int num1 = 0;
+        int num2 = 0;
+
+        for (int i = 101; i < 1000; i++)
+        {
+            for (int j = 101; j < 1000; j++)
+            {
+                int product = i * j;
+                if (IsPalindrome(product) && product > largestProduct)
+                {
+                    largestProduct = product;
+                    num1 = i;
+                    num2 = j;
+                }
+            }
+        }
+
+        return $"{num1} * {num2} = {largestProduct}";
+    }
+
+    public bool IsPalindrome(int number)
+    {
+        string numberString = number.ToString();
+        int length = numberString.Length;
+        for (int i = 0; i < length / 2; i++)
+        {
+            if (numberString[i] != numberString[length - i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
