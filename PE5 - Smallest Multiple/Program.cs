37c5f6a6691102137e8467af3e083ca8dd12c5a4
Jacob Snyder <95860834+jsGH314@users.noreply.github.com>
2024-09-27 11:42:46 -0500

    init commit for PE5

---
+++ PE5 - Smallest Multiple/Program.cs
@@ -0,0 +1,34 @@
+﻿public class PE5
+{
+    // O(n) solution
+    static void Main(string[] args)
+    {
+        PE5 pe5 = new PE5();
+        System.Console.WriteLine(pe5.SmallestMultiple());
+    }
+
+    public int SmallestMultiple()
+    {
+        foreach (var number in Enumerable.Range(1, int.MaxValue))
+        {
+            if (IsDivisibleByAll(number))
+            {
+                return number;
+            }
+        };
+        return 0;
+    }
+
+    public bool IsDivisibleByAll(int number)
+    {
+        for (int i = 1; i <= 20; i++)
+        {
+            if (number % i != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
